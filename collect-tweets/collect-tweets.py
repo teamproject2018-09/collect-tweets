66ec28d5824cc57a34dde5ee7a3f25736734c5a7
Erik Tjong Kim Sang <erikt@xs4all.nl>
2018-09-25 18:35:01 +0200

    added command line option

--- collect-tweets/collect-tweets.py
+++ collect-tweets/collect-tweets.py
@@ -1,7 +1,10 @@
-import tweepy
 import definitions
+import json
+import sys
+import tweepy
 
-USER = "esciencecenter"
+TASK = "getUserTweets" # getUserTweets or searchForTweets
+COMMAND = sys.argv.pop(0)
 NBROFTWEETS = 200
 
 def authenticate():
@@ -12,34 +15,41 @@ def authenticate():
                           definitions.token_secret)
     return(auth)
 
+# 20180925 not used yet
 def limit_handled(cursor):
     while True: 
         try: yield cursor.next()
         except tweepy.RateLimitError: time.sleep(15 * 60)
 
-def getAllTweets(api,userName,count):
-    tweetList = []
-    for status in limit_handled(tweepy.Cursor(api.user_timeline,
-                                              screen_name=userName,
-                                              count=count)).items(): 
-        tweetList.append(status._json)
-    return(tweetList)
+def convertListToJson(thisList):
+    return([json.dumps(json_obj) for json_obj in thisList])
+
+def getUserTweets(api,userName,count):
+    tweetList = [status._json for status in tweepy.Cursor(api.user_timeline,
+                                                          screen_name=userName,
+                                                          count=count,
+                                                          tweet_mode="extended").items()] 
+    return(convertListToJson(tweetList))
 
 def searchForTweets(api,query,count):
-    tweetList = []
-    for status in tweepy.Cursor(api.search,
-                                q=query,
-                                count=count).items(): 
-        tweetList.append(status._json)
-    return(tweetList)
+    tweetList = [status._json for status in tweepy.Cursor(api.search,
+                                                          q=query,
+                                                          count=count,
+                                                          tweet_mode="extended").items()] 
+    return(convertListToJson(tweetList))
 
 def printList(thisList):
     for t in thisList: print(t)
     return()
 
+if len(sys.argv) < 1: sys.exit("usage: "+COMMAND+" user")
+user = sys.argv.pop(0)
+
 auth = authenticate()
 api = tweepy.API(auth)
-# tweets = getAllTweets(api,USER,NBROFTWEETS)
-tweets = searchForTweets(api,USER,NBROFTWEETS)
+tweets = []
+if TASK == "getUserTweets": tweets = getUserTweets(api,user,NBROFTWEETS)
+elif TASK == "searchForTweets": tweets = searchForTweets(api,user,NBROFTWEETS)
+else: sys.exit(COMMAND+": unknown task!")
 printList(tweets)
 
