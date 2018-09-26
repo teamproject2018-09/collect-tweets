bf7a2f3a68d7981251d2fdc8b50d8ab756f2c903
Erik Tjong Kim Sang <erikt@xs4all.nl>
2018-09-26 17:27:20 +0200

    expanded output columns

--- collect-tweets/process-html.py
+++ collect-tweets/process-html.py
@@ -5,7 +5,7 @@ import sys
 import time
 
 COMMAND = sys.argv.pop(0)
-FIELDNAMES = ["id","date","user","hashtags","mentions","text"]
+FIELDNAMES = ["id","date","time","datetime","user","hashtags","mentions","reply-count","retweet-count","favorites-count","text"]
 
 def getAttribute(tag,attrName):
     if attrName in tag.attrs: return(tag[attrName])
@@ -30,16 +30,38 @@ with sys.stdout as csvfile:
                     text = getText(div,"p")
                     text = patternNewline.sub(" ",text)
                     date = ""
+                    timeString = ""
+                    datetimeString = ""
                     for span in div.find_all("span"): 
                         tmpDate = getAttribute(span,"data-time")
                         if tmpDate != "": 
-                            date = time.asctime(time.gmtime(int(tmpDate)))
+                            date = time.strftime("%Y-%m-%d",time.gmtime(int(tmpDate)))
+                            timeString = time.strftime("%H:%M:%S",time.gmtime(int(tmpDate)))
+                            datetimeString = date+"T"+timeString+"+00:00"
                     hashtagList = []
                     for a in div.find_all("a"):
                         if "class" in a.attrs:
                             for i in range(0,len(a["class"])):
                                 if a["class"][i] == "twitter-hashtag":
                                     hashtagList.append(getText(a,"b"))
+                    retweetCount = 0
+                    favoritesCount = 0
+                    replyCount = 0
+                    for span1 in div.find_all("span"):
+                        if "class" in span1.attrs:
+                            for i in range(0,len(span1["class"])):
+                                if span1["class"][i] == "ProfileTweet-action--retweet":
+                                    for span2 in span1.find_all("span"):
+                                        if "data-tweet-stat-count" in span2.attrs:
+                                            retweetCount = span2["data-tweet-stat-count"]
+                                if span1["class"][i] == "ProfileTweet-action--favorite":
+                                    for span2 in span1.find_all("span"):
+                                        if "data-tweet-stat-count" in span2.attrs:
+                                            favoritesCount = span2["data-tweet-stat-count"]
+                                if span1["class"][i] == "ProfileTweet-action--reply":
+                                    for span2 in span1.find_all("span"):
+                                        if "data-tweet-stat-count" in span2.attrs:
+                                            replyCount = span2["data-tweet-stat-count"]
                     hashtags = " ".join(hashtagList)
-                    outFile.writerow({"id":tweetId,"date":date,"user":screenName,"hashtags":hashtags,"mentions":mentions,"text":text})
+                    outFile.writerow({"id":tweetId,"date":date,"time":timeString,"datetime":datetimeString,"user":screenName,"hashtags":hashtags,"mentions":mentions,"reply-count":replyCount,"retweet-count":retweetCount,"favorites-count":favoritesCount,"text":text})
 
--- collect-tweets/process-json.py
+++ collect-tweets/process-json.py
@@ -1,10 +1,12 @@
 import csv
+from datetime import datetime
 import json
 import re
 import sys
 
 COMMAND = sys.argv[0]
-FIELDNAMES = ["id","date","user","hashtags","mentions","text"]
+DATEFORMAT = "%a %b %d %H:%M:%S %z %Y"
+FIELDNAMES = ["id","date","time","datetime","user","hashtags","mentions","reply-count","retweet-count","favorites-count","text"]
 
 patternNewline = re.compile(r"\n")
 patternTimezone = re.compile(r" .\d\d\d\d ")
@@ -17,13 +19,19 @@ with sys.stdout as csvfile:
            "user" in jsonLine and "screen_name" in jsonLine["user"]:
             tweetId = jsonLine["id_str"]
             date = jsonLine["created_at"]
-            date = patternTimezone.sub(" ",date)
+            date = datetime.strptime(jsonLine["created_at"],DATEFORMAT)
+            timeString = date.strftime("%H:%M:%S")
+            date = date.strftime("%Y-%m-%d")
+            datetimeString = date+"T"+timeString+"+00:00"
             screenName = jsonLine["user"]["screen_name"]
             text = jsonLine["full_text"]
             text = patternNewline.sub(" ",text)
+            replyCount = "0" # unavailable in json
+            retweetCount = jsonLine["retweet_count"]
+            favoritesCount = jsonLine["favorite_count"]
             hashtagList = [e["text"] for e in jsonLine["entities"]["hashtags"]]
             hashtags = " ".join(hashtagList)
             mentionList = [e["screen_name"] for e in jsonLine["entities"]["user_mentions"]]
             mentions = " ".join(mentionList)
-            outFile.writerow({"id":tweetId,"date":date,"user":screenName,"hashtags":hashtags,"mentions":mentions,"text":text})
+            outFile.writerow({"id":tweetId,"date":date,"time":timeString,"datetime":datetimeString,"user":screenName,"hashtags":hashtags,"mentions":mentions,"reply-count":replyCount,"retweet-count":retweetCount,"favorites-count":favoritesCount,"text":text})
     csvfile.close()
